ae2d681e9471229ad5927cd44dae2109bd09c2de
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-12-18 06:16:31 -0300

    feat: criando o model authors

--- src/entities/Autor.ts
+++ src/entities/Autor.ts
@@ -1,5 +1,30 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('authors')
 class Autor {
-    
+   @PrimaryGeneratedColumn('increment')
+    id: string;
+
+    @Column('varchar',{ length: 255, nullable: false })
+    name: string;
+
+    @Column('date',{ nullable: false })
+    birthdate: Date;
+
+    @Column('text',{ nullable: false })
+    biography: string;
+
+    @Column('varchar',{ length: 100, nullable: false })
+    nationality: string;
+
+    @Column('boolean',{ default: true })  
+    active: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
 }
 
 export default Autor;
